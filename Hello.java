d8c17f029e046b40e39e4659b6af91856b869228
Sufwan Amir <sufwanamir99@gmail.com>
2026-09-23 12:39:22 +0500

    Initial Commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+	public static void main(String[] args) {
+        System.out.println("Hello from:");
+        System.out.println("Sufwan Amir Zuberi");
+        System.out.println("Roll Number: FA23-BDS-063");
+
+    }
+}
