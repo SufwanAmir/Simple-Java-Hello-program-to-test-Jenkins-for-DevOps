8b21d0b997f7eb0903305288a4fb81ef51f87646
Sufwan Amir <sufwanamir99@gmail.com>
2026-09-24 14:09:08 +0500

    Hello to Sir Qasim

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
         System.out.println("Hello from:");
         System.out.println("Sufwan Amir Zuberi");
         System.out.println("Roll Number: FA23-BDS-063");
-        System.out.println("New Event Test");
+        System.out.println("Hello Sir Qasim! Nice to see you.");
 
     }
 }
